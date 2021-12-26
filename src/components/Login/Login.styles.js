84e85d711f7ac21379aa1f3b68dc362fbfcb3f70
Bogdan <bogdan_kolesov@bk.ru>
2021-12-26 21:37:59 +0600

    fix: Login

--- src/components/Login/Login.styles.js
+++ src/components/Login/Login.styles.js
@@ -1,2 +1,16 @@
 import styled from 'styled-components';
 
+export const Container = styled.section`
+    position: relative;
+    height: calc(100vh - 70px);
+    &:before{
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        content: '';
+        background-image: url('/assets/images/login-background.jpg');
+    }
+`
+

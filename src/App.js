db5c4b3949513c4438c15f3b8b4bf58ae4e1d0b3
Bogdan <bogdan_kolesov@bk.ru>
2021-12-26 21:30:43 +0600

    add: Login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import { GlobalTheme } from './themes/GlobalTheme';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Detail from './components/Detail';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/detail' element={<Detail />} />
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </Router >
   );

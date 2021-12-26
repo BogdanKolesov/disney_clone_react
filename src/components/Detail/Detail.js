383739569ae3ded8a31700e3d78eff687589f7ee
Bogdan <bogdan_kolesov@bk.ru>
2021-12-26 16:43:36 +0600

    add: Detail

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Container, Background, ImageTitle, Controls, PlayButton, TrailerButton, AddButton, GroupWatchButton } from './Detail.styled';
 
 const Detail = () => {
     return (
-        <div>
-            Detail
-        </div>
+        <Container>
+            <Background>
+                <img src='https://cdn.pixabay.com/photo/2016/01/14/20/27/fan-art-1140718_1280.png' alt='' />
+            </Background>
+            <ImageTitle>
+                <img src='https://cdn.pixabay.com/photo/2021/05/03/14/30/simpsons-6226380_1280.png' alt='' />
+            </ImageTitle>
+            <Controls>
+                <PlayButton>
+
+                </PlayButton>
+                <TrailerButton>
+
+                </TrailerButton>
+                <AddButton>
+
+                </AddButton>
+                <GroupWatchButton>
+
+                </GroupWatchButton>
+            </Controls>
+        </Container>
     );
 }
 
--- src/components/Detail/Detail.styled.js
+++ src/components/Detail/Detail.styled.js
@@ -1,1 +1,52 @@
 import styled from 'styled-components';
+
+export const Container = styled.section`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+    position: relative;
+`
+export const Background = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+    opacity: 0.8;
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`
+
+export const ImageTitle = styled.div`
+    height: 30vh;
+    width: 35vw;
+    min-height: 170px;
+    min-width: 200px;
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+`
+
+export const Controls = styled.div`
+
+`
+export const PlayButton = styled.div`
+
+`
+
+export const TrailerButton = styled.div`
+
+`
+
+export const AddButton = styled.div`
+
+`
+
+export const GroupWatchButton = styled.div`
+
+`

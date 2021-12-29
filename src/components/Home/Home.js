e35c3bcf1ed014703cd9bfdcce7d15d0a5de707e
Bogdan <bogdan_kolesov@bk.ru>
2021-12-29 13:13:16 +0600

    add: redux

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from './Home.styles'
 import ImageSlider from '../ImageSlider'
 import { imageSliderData } from '../../data/imageSliderData';
@@ -6,8 +6,18 @@ import Viewers from '../Viewers';
 import { viewersData } from '../../data/viewersData'
 import Movies from '../Movies'
 import { moviesData } from '../../data/moviesData'
+import db from '../../firebase'
 
 const Home = () => {
+
+    useEffect(() => {
+        db.collection('movies').onSnapshot((snapshot) => {
+            let tempMovies = snapshot.docs.map((doc) => {
+                return { id: doc.id, ...doc.data() }
+            })
+        })
+    }, [])
+
     return (
         <Container>
             <ImageSlider content={imageSliderData} />
